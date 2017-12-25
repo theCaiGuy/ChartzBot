df716effb8e4d39e514a40fc2ea2d39e1f12a964
theCaiGuy <michaelcai09@gmail.com>
2017-12-25 17:49:27 -0600

    Small change to see what it does

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = "Not so cool now am I";
 
   options = {
     hostname: 'api.groupme.com',
