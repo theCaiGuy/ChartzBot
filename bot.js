e8448db0af6c249895e1644a1579a8bff4149172
theCaiGuy <michaelcai09@gmail.com>
2017-12-25 22:26:36 -0600

    Added more commands

--- bot.js
+++ bot.js
@@ -21,9 +21,15 @@ function respond() {
 function postMessage(request) {
   var botResponse, options, body, botReq;
 
-  if (request.text.length <= 8) botResponse = "Usage: Show me <song title>";
-  else botResponse = "Here's your song: " + request.text.substring(8);
-
+  if (request.text.length <= 8) botResponse = "Usage: 'Show me <song title> | help'";
+  else {
+    song_title = request.text.substring(8);
+    if (song_title == "help") {
+      botResponse = "Here's what I've got: "
+    } else {
+      botResponse = "Here's your song: " + request.text.substring(8);
+    }
+  }
   options = {
     hostname: 'api.groupme.com',
     path: '/v3/bots/post',
