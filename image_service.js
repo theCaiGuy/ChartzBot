66e3091320fc6157074c5be75a7c1be4e62d57f5
theCaiGuy <michaelcai09@gmail.com>
2017-12-26 17:05:17 -0600

    Supposedly linked up a mongodb database for image urls

--- image_service.js
+++ image_service.js
@@ -2,8 +2,19 @@ export class ImageService {
     HTTPS = require('https');
     botID = process.env.BOT_ID;
     authID = process.env.GROUPME_AUTH;
+    MongoClient = require('mongodb').MongoClient;
+    assert = require('assert');
+    mongo_name = 'ds131687/heroku_ht08rkq2'
+    db;
 
-    constructor() {}
+    constructor() {
+        MongoClient.connect(process.env.MONGODB_URI, function(err, client) {
+            assert.equal(null, err);
+            console.log("Connected successfully to mongodb");
+            db = client.db(mongo_name);
+            client.close();
+        });
+    }
 
     getImageURL(imagePath) {
         options = {
