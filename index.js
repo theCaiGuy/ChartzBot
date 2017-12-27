ae8c22dd3b3161e930fdd357c7cd01832729fc49
theCaiGuy <michaelcai09@gmail.com>
2017-12-27 00:20:28 -0600

    Aesthetics

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ server.listen(port);
 
 function ping() {
   this.res.writeHead(200);
-  this.res.end("Looks like you've found LSJUMB Altoz Practice Bot!");
+  this.res.end("Looks like you've found LSJUMB Altoz Practice Bot! Hooray for you! Altoz<3");
 }
