084e6cfa6bcd202e41d5c6250dcf46cba1e01584
theCaiGuy <michaelcai09@gmail.com>
2017-12-26 18:12:08 -0600

    test

--- image_service.js
+++ image_service.js
@@ -1,3 +1,7 @@
+var HTTPS = require('https');
+var botID = process.env.BOT_ID;
+var authID = process.env.GROUPME_AUTH;
+
 module.exports = {
     getURL: function(imageName) {
         return "https://i.groupme.com/480x325.jpeg.9e20b71dd6af4b58bbd132d4a7dec009";
