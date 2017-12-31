b03bc8840f31f548b5c8f831722354c39563f55e
theCaiGuy <michaelcai09@gmail.com>
2017-12-30 23:32:18 -0600

    Move location slightly to avoid confusing future frosh

--- bot.js
+++ bot.js
@@ -105,8 +105,8 @@ function handleLocation(body, options) {
   body.text = "Be there or be square";
   body.attachments = [{
     "type" : "location",
-    "lat" : "37.431",
-    "lng" : "-122.161",
+    "lat" : "37.4316286",
+    "lng" : "-122.16174",
     "name" : "Shak"
   }];
   postMessage(body, options);
