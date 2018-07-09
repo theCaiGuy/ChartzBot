c1ad6bf42f9619d068b47ec73ef882d4ec080626
theCaiGuy <michaelcai09@gmail.com>
2018-07-08 21:55:26 -0500

    Testing spell check again

--- image_service.js
+++ image_service.js
@@ -23,25 +23,27 @@ module.exports = {
     },
 
     getPossibleSpellings: function(imageName) {
-        var possible_corrections = [];
-        for (var i = 0; i < imageName.length; i++) {
-            // Check deletions
-            possible_deletion = imageName.substring(0, i) + imageName.substring(i + 1);
-            possible_corrections.push(possible_deletion);
-            // Check letter swap
+        // var possible_corrections = [];
+        // for (var i = 0; i < imageName.length; i++) {
+        //     // Check deletions
+        //     possible_deletion = imageName.substring(0, i) + imageName.substring(i + 1);
+        //     possible_corrections.push(possible_deletion);
+        //     // Check letter swap
 
-            for (var letter = 'a'; letter < 'z'; letter++) {
-                // Check insertions
+        //     for (var letter = 'a'; letter < 'z'; letter++) {
+        //         // Check insertions
 
-                // Check substitutions
-            }
-        }
+        //         // Check substitutions
+        //     }
+        // }
+        // var possible_spellings = [];
+        // for (var j = 0; j < possible_corrections.length; j++) {
+        //     if (this.getURL(possible_corrections[j] != "") {
+        //         possible_spellings.push(possible_corrections[j]);
+        //     }
+        // }
         var possible_spellings = [];
-        for (var j = 0; j < possible_corrections.length; j++) {
-            if (this.getURL(possible_corrections[j] != "") {
-                possible_spellings.push(possible_corrections[j]);
-            }
-        }
+        possible_spellings.push("Careless Whisper");
         return possible_spellings;
     }
 };
